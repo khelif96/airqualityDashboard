3310ad92ae8b83887570b0d74774f64a0491b9a6
Mohamed Khelif <mohamed.khelif@mongodb.com>
2023-04-03 07:00:37 -0400

    Create dashboard

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,42 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Status from './Status.svelte';
+	let hasData = false;
+	let time = '';
+	let co2 = 0;
+	let tvoc = 0;
+	const fetchData = async () => {
+		hasData = false;
+		const response = await fetch(
+			'https://data.mongodb-api.com/app/roomstats-aptyc/endpoint/getLatest'
+		);
+		const jsonData = await response.json();
+		hasData = true;
+		co2 = parseInt(jsonData[0]['CO2'], 10);
+		tvoc = parseInt(jsonData[0]['TVOC'], 10);
+		time = new Date(jsonData[0]['time']).toTimeString();
+	};
+
+	fetchData();
+</script>
+
+<main>
+	{#if hasData}
+		<h1>TVOC: <Status current={tvoc} warning={5000} danger={10000} unit="PPB" /></h1>
+		<h1>CO2: <Status current={co2} warning={2000} danger={3000} unit="PPM" /></h1>
+
+		<p>Last Update: {time}</p>
+	{/if}
+	{#if !hasData}
+		<h1>Loading...</h1>
+	{/if}
+
+	<button on:click={fetchData}>Refresh</button>
+</main>
+
+<style>
+	main {
+		font-family: sans-serif;
+		text-align: center;
+		margin: auto;
+	}
+</style>
---
+++ src/routes/Status.svelte
@@ -0,0 +1,19 @@
+<script>
+	export let current = 0;
+	export let warning = 50;
+	export let danger = 75;
+	export let unit = '';
+
+	let textColor = 'green';
+
+	$: if (current < warning) {
+		textColor = 'green';
+	} else if (current >= warning && current < danger) {
+		textColor = 'orange';
+	} else {
+		textColor = 'red';
+	}
+</script>
+
+<span style="color: {textColor}">{current}</span>
+{unit}
